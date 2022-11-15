2faec938c572de8da2358a21592e266de6eb9243
= <=>
2022-11-15 10:09:24 -0500

    ass WelcomePage test

---
+++ test/ui/welcome/welcome_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kripton/ui/welcome/welcome_page.dart';
+
+Future<void> main() async {
+  testWidgets('WelcomePage content test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: WelcomePage()));
+    expect(find.image(const AssetImage('assets/images/logo_white.png')), findsOneWidget);
+    expect(find.text('Comenzar ahora mismo'), findsOneWidget);
+    expect(find.text('Te damos la bienvenida a ser parte de Urbano, empresa líder en distribución logistica de última milla, con Kriptón HD podrás gestionar tus pedidos.'), findsOneWidget);
+    expect(find.text('SIGUIENTE'), findsOneWidget);
+  });
+}
